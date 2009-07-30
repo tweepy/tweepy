cf04a70385e2154f1583415cb1f807dd9a877d25
Josh Roesslein <jroesslein@gmail.com>
2009-07-30 00:07:26 -0500

    Rewrote example.

--- example.py
+++ example.py
@@ -1,11 +1,27 @@
-from api import API
+import tweepy
 
-# Create API object
-api = API()
+print 'An example of using the Tweepy library...'
 
-# Fetch public timeline
-p_timeline = api.public_timeline()
+# Some twitter credentials.
+# Fill these in with your own login credentials.
+username = 'jitterapp'
+password = 'omega123'
 
-# Print texts of statuses
-for status in p_timeline:
-  print status.text
+# Create an instance of the API object.
+api = tweepy.API(username, password)
+
+# Let's get a list of the statuses on the public timeline
+# and print the texts to the console.
+public_timeline = api.public_timeline()
+for status in public_timeline:
+  print '%s:  %s\n   from %s posted at %s' % (
+      status.user.screen_name, status.text, status.source, status.created_at)
+
+# Now we will update our twitter status
+# and print the text to the console.
+update = api.update_status(status='hello!')
+print 'Update: %s' % update.text
+
+# Get the timeline for the 'twitter' user.
+twitter_timeline = api.user_timeline(screen_name='twitter')
+
