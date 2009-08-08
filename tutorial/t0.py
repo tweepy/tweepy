820d423ce85634e01bb6e8af467ce7ab0fda05d8
Josh Roesslein <jroesslein@gmail.com>
2009-08-08 14:42:26 -0500

    Added tutorial 2 and introduction.

---
+++ tutorial/t0.py
@@ -0,0 +1,14 @@
+""" Introduction
+
+This series of tutorials is designed to introduce you to key
+concepts of Tweepy. By the end of this series you should be ready
+to create your grand Twitter application with Tweepy!
+
+If you get lost on your journey to becoming a Tweepy guru, feel
+free to post questions to tweepy@googlegroups.com
+
+Each tutorial file is named tX.py where X is the tutorial number.
+Work your way from 1 and up.
+
+Author: Joshua Roesslein
+"""
