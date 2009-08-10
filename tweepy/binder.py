e22e387b74a61dd3b9c6d3fea17ce718b60d03ab
Josh Roesslein <jroesslein@gmail.com>
2009-08-10 11:25:46 -0500

    Fix paramter validation.

--- tweepy/binder.py
+++ tweepy/binder.py
@@ -27,8 +27,12 @@ def bind_api(path, parser, allowed_param=None, method='GET', require_auth=False,
       for k, arg in kargs.items():
         if k in parameters:
           raise TweepError('Multiple values for parameter %s supplied!' % k)
+        if k not in allowed_param:
+          raise TweepError('Invalid parameter %s supplied!' % k)
         parameters[k] = arg
     else:
+      if len(args) > 0 or len(kargs) > 0:
+        raise TweepError('This method takes no parameters!')
       parameters = None
 
     # Assemble headers
