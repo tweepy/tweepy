778d88bb8c2472278386e80a420ef8a37b70d036
Josh Roesslein <jroesslein@gmail.com>
2010-10-16 23:03:15 -0500

    Use unicode for Tweepy error messges.
    Closes #38

--- tweepy/error.py
+++ tweepy/error.py
@@ -6,7 +6,7 @@ class TweepError(Exception):
     """Tweepy exception"""
 
     def __init__(self, reason, response=None):
-        self.reason = str(reason)
+        self.reason = unicode(reason)
         self.response = response
 
     def __str__(self):
