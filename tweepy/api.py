3052a9eccfe214be3c048d2df5b3ff31351a4a64
Mike <mikeandmore@gmail.com>
2011-01-23 14:21:53 +0800

    add timeout

--- tweepy/api.py
+++ tweepy/api.py
@@ -18,7 +18,7 @@ class API(object):
             host='api.twitter.com', search_host='search.twitter.com',
              cache=None, secure=False, api_root='/1', search_root='',
             retry_count=0, retry_delay=0, retry_errors=None,
-            parser=None):
+            parser=None, timeout=None):
         self.auth = auth_handler
         self.host = host
         self.search_host = search_host
@@ -26,6 +26,7 @@ class API(object):
         self.search_root = search_root
         self.cache = cache
         self.secure = secure
+        self.timeout = timeout
         self.retry_count = retry_count
         self.retry_delay = retry_delay
         self.retry_errors = retry_errors
