4cb092233470453df0477d4bbd7a00695d760dd1
Tiberiu Florea <tiberiu.florea@ubervu.com>
2013-01-21 16:11:32 +0200

    [binder.py] moved code inside try-except block

--- tweepy/binder.py
+++ tweepy/binder.py
@@ -159,17 +159,17 @@ def bind_api(**config):
                 time.sleep(self.retry_delay)
                 retries_performed += 1
 
-            # If an error was returned, throw an exception
-            self.api.last_response = resp
-            if resp.status != 200:
-                try:
-                    error_msg = self.api.parser.parse_error(resp.read())
-                except Exception:
-                    error_msg = "Twitter error response: status code = %s" % resp.status
-                raise TweepError(error_msg, resp)
+                # If an error was returned, throw an exception
+                self.api.last_response = resp
+                if resp.status != 200:
+                    try:
+                        error_msg = self.api.parser.parse_error(resp.read())
+                    except Exception:
+                        error_msg = "Twitter error response: status code = %s" % resp.status
+                    raise TweepError(error_msg, resp)
 
-            # Parse the response payload
-            result = self.api.parser.parse(self, resp.read())
+                # Parse the response payload
+                result = self.api.parser.parse(self, resp.read())
 
             conn.close()
 
