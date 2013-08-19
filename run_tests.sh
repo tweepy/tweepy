e4de0e659b76d546483e21718155bb40352f7076
Joshua Roesslein <jroesslein@gmail.com>
2013-08-19 12:28:16 -0700

    Fix record.json download link.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 if [[ $TRAVIS_SECURE_ENV_VARS == "false" ]]; then
-  curl https://www.dropbox.com/s/zsvzl6sl8gw8o6y/record.json -o tests/record.json
+  curl "https://dl.dropboxusercontent.com/u/231242/record.json" -o tests/record.json
   USE_REPLAY=1 nosetests -v tests.test_api tests.test_utils
 else
   nosetests -v --with-coverage tests.test_api tests.test_streaming tests.test_cursors tests.test_utils
