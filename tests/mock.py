e44bf2a3335c3e90c6280533a1136236a91af766
Timo Ewalds <tewalds@google.com>
2013-08-26 17:23:01 -0400

    Add tests for the streaming connection backoffs.
    Move mock.py contents into test_utils.py to fix conflict between local mock.py and mock library.

--- tests/mock.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def mock_tweet():
-    """Generate some random tweet text."""
-    count = random.randint(70, 140)
-    return ''.join([random.choice(string.letters) for i in xrange(count)])
-
