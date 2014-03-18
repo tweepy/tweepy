478585940b0300b7c2995951e237d720db6f278c
Joshua Roesslein <jroesslein@opengov.com>
2014-03-17 21:05:38 -0700

    Only run tests agains Twitter API on production branch.

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-if [[ $TRAVIS_SECURE_ENV_VARS == "false" ]]; then
+if [[ $TRAVIS_BRANCH == "production" ]]; then
+  nosetests -v --with-coverage tests.test_api tests.test_streaming tests.test_cursors tests.test_utils
+else
   curl "https://dl.dropboxusercontent.com/u/231242/record.json" -o tests/record.json
   USE_REPLAY=1 nosetests -v tests.test_api tests.test_utils
-else
-  nosetests -v --with-coverage tests.test_api tests.test_streaming tests.test_cursors tests.test_utils
 fi
