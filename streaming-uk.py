97821645abdce8e198b346607f6ff77b77fb0e0d
Robinlovelace <rob00x@gmail.com>
2014-05-27 00:36:02 +0100

    saving tweets into single file, processing json with R

--- streaming-uk.py
+++ streaming-uk.py
@@ -4,12 +4,11 @@ from tweepy import Stream
 import configparser # for reading the configuration file
 import os
 import sys
-import json # For converting string into json object
 
-consumer_key="UDSAVzq7Jn0mpUn12rGWiw"
-consumer_secret="BLj5stYAMdY09Tk3iTEYQjcNXNreQcA3z9EQbqmQU"
-access_token="80250475-0KagN1PxplwmR1ZDg15t71Jr7D8rVouuxq2j32J1n"
-access_token_secret="kkQxyLuDj4ezFlg0H5UpcBh3sEwUJiowk5AvpJbMUC2bb"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
 
 
 class StdOutListener(StreamListener):
@@ -31,35 +30,17 @@ class FileWriterListener(StreamListener):
     A listener handles tweets are the received from the stream, writing them to a file
     """
 
-    def on_data(self, raw_data):
+    def on_data(self, data):
 
-        # Call the parent (StreamReader) function which does some error checking, returning False if
-        # this isn't a tweet
-        # if super(StreamListener, self).on_data(raw_data) == False:
-        #    print "This doesn't look like a tweet"
-        #    return False
+        # DATA IS A STRING, NEEDS TO BE CONVERTED INTO A PYTHON DICTIONARY BEFORE BEING INTERROGATED
 
-        # 1 - use json library to create a python dictionary object from the raw data (a 
-        # json-formatter string). This can be then be interrogated to find info. about the tweet.
-
-        data = json.loads(raw_data)
+        # 1 - use json library to create a json object
 
         # 2 - get the id (e.g. data['id'] )
 
-        tweetid = str(data['id'])
-        print "read tweet",tweetid
-
         # 3 - write to a file (with filename of tweet id)
-        f = open('data/tweets.json','a')
-        try:
-            f.write(raw_data) # 
-        finally:
-            f.close()
 
-        # P.S. this is a nicer way to write to files using 'with' syntax:
-        #with open('data/'+tweetid,'w') as f:
-        #    f.write(str(data))
-
+        print "read tweet"
         return True
 
     def on_error(self, status):
@@ -71,12 +52,12 @@ class FileWriterListener(StreamListener):
 if __name__ == '__main__':
     # Read the twitter authentication stuff from the configuration file (see README for details).
     try:
-        if not os.path.isfile('~/Desktop/credentials.ini'):
+        if not os.path.isfile('credentials.ini'):
             print "Error, there is no credentials.ini file. See the README for details."
             sys.exit()
 
         config = configparser.ConfigParser()
-        config.read('~/Desktop/credentials.ini')
+        config.read('/home/robin/Dropbox/configs/credentials.ini')
 
         consumer_key=str(config['CREDENTIALS']['consumer_key'])
         consumer_secret=str(config['CREDENTIALS']['consumer_secret'])
@@ -94,4 +75,5 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(locations=[-2.17,53.52,-1.20,53.96])
+    #stream.filter(locations=[-2.17,53.52,-1.20,53.96])
+    stream.filter(locations=[-10,50,2,60])
