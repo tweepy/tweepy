caf15a92a035dc1ddce4e066662209e2a4d25984
Katsunori SUZUI <kt@7kry.net>
2014-06-13 18:29:45 +0900

    Fix tweepy.API.update_with_madia
    `filename` have to be percent-encoded whichever it is an instance of
    `unicode` or an instance of `str`.

--- tweepy/api.py
+++ tweepy/api.py
@@ -790,7 +790,8 @@ class API(object):
             raise TweepError('Invalid file type for image: %s' % file_type)
 
         if isinstance(filename, unicode):
-          filename = urllib.quote(filename.encode("utf-8"))
+          filename = filename.encode("utf-8")
+        filename = urllib.quote(filename)
 
 
         BOUNDARY = 'Tw3ePy'
