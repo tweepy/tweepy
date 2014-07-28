05b5a4390c7109bb8c0778c547883c41272769fb
Mickaël Fosso <jazzkeats@gmail.com>
2014-07-28 16:37:27 +0200

    Change to make sure tweepy use SSL
    I had some problems using Tweepy and I realized it was because it didn't always use SSL. So I propose to add a line to make sure the connexion we use is secure.

--- examples/oauth.py
+++ examples/oauth.py
@@ -17,6 +17,7 @@ access_token=""
 access_token_secret=""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.secure = True
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
