84cac5f5702014767cfd04abce41bcfd1e3f93d9
Mark Smith <mark.smith@practicalpoetry.co.uk>
2014-08-16 10:01:16 +0100

    Removed download of old httreplay recording from test run.

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,5 @@
 if [[ $TRAVIS_BRANCH == "production" ]]; then
   nosetests -v --with-coverage tests.test_api tests.test_streaming tests.test_cursors tests.test_utils
 else
-  curl "https://dl.dropboxusercontent.com/u/231242/record.json" -o tests/record.json
   tox
 fi
