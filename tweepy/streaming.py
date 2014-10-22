c841a09a0a05b27979a5768274735d6fde184ea2
Timo Ewalds <tewalds@google.com>
2014-10-21 20:18:09 -0400

    Treat IncompleteRead as a disconnect or timeout, and reconnect.

--- tweepy/streaming.py
+++ tweepy/streaming.py
@@ -182,7 +182,7 @@ class Stream(object):
                     self.snooze_time = self.snooze_time_step
                     self.listener.on_connect()
                     self._read_loop(resp)
-            except (Timeout, ssl.SSLError) as exc:
+            except (Timeout, ssl.SSLError, requests.compat.IncompleteRead) as exc:
                 # This is still necessary, as a SSLError can actually be
                 # thrown when using Requests
                 # If it's not time out treat it like any other exception
