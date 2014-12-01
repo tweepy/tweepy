f9b381105d4ca249b85522107ddedd8c1368c32e
Yuri Prezument <y@yprez.com>
2014-12-01 15:53:27 +0200

    Regression test for #518

--- tests/test_cursors.py
+++ tests/test_cursors.py
@@ -43,3 +43,13 @@ class TweepyCursorTests(TweepyTestCase):
         self.assertEqual(c.iterator.next_cursor, 123456)
         self.assertFalse('cursor' in c.iterator.kargs)
 
+    @tape.use_cassette('testcursornext.json')
+    def testcursornext(self):
+        """
+        Test cursor.next() behavior, id being passed correctly.
+        Regression test for issue #518
+        """
+        cursor = Cursor(self.api.user_timeline, id='twitter').items(5)
+        status = cursor.next()
+
+        self.assertEquals(status.user.screen_name, 'twitter')
