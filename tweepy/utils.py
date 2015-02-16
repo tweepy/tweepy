88172bf720bfbd71af8c91b9e81075a03b35f9dd
Joshua Roesslein <jroesslein@opengov.com>
2015-02-16 11:36:58 -0800

    Remove urlencode_noplus util method.

--- tweepy/utils.py
+++ tweepy/utils.py
@@ -56,9 +56,3 @@ def import_simplejson():
 def list_to_csv(item_list):
     if item_list:
         return ','.join([str(i) for i in item_list])
-
-
-def urlencode_noplus(query):
-    return '&'.join(['%s=%s' % (quote(str(k), ''), quote(str(v), ''))
-                    for k, v in query.items()])
-
