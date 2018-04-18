778bd7a31d2f5fae98652735e7844533589ca221
Ben Cipollini <bcipollini@classy.org>
2018-04-18 10:26:07 -0700

    #1029 - migrate requirements to setup.py, reference in requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 #from distutils.core import setup
 import re, uuid
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 VERSIONFILE = "tweepy/__init__.py"
 ver_file = open(VERSIONFILE, "rt").read()
@@ -14,9 +13,6 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(req.req) for req in install_reqs]
-
 setup(name="tweepy",
       version=version,
       description="Twitter library for python",
@@ -25,7 +21,12 @@ setup(name="tweepy",
       author_email="tweepy@googlegroups.com",
       url="http://github.com/tweepy/tweepy",
       packages=find_packages(exclude=['tests']),
-      install_requires=reqs,
+      install_requires=[
+          "requests>=2.11.1",
+          "requests_oauthlib>=0.7.0",
+          "six>=1.10.0",
+          "PySocks>=1.5.7",
+      ],
       keywords="twitter library",
       classifiers=[
           'Development Status :: 4 - Beta',
