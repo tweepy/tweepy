301706261e49fe5f2eb9dc48dd34c8ab0e248871
Hugo <hugovk@users.noreply.github.com>
2018-05-14 16:06:16 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(name="tweepy",
           "PySocks>=1.5.7",
       ],
       keywords="twitter library",
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Topic :: Software Development :: Libraries',
