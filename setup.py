25899b5926564ad0febdb019da0fb1bc46c86726
Harmon <Harmon758@gmail.com>
2019-05-04 13:28:53 -0500

    Update setup Development Status classifier

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(name="tweepy",
       keywords="twitter library",
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Topic :: Software Development :: Libraries',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
