ccee031da083424b994a3f47472aa0faf20d69b0
Harmon <Harmon758@gmail.com>
2019-06-21 14:22:45 -0500

    Exclude Python 3.4 from setup python_requires

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name="tweepy",
           "six>=1.10.0",
       ],
       keywords="twitter library",
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Topic :: Software Development :: Libraries',
