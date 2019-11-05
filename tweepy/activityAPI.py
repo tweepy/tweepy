89c63cf4b984af0deea298c247f8bd2c9054c2c5
Simon Lucy <simon.lucy@gravit.as>
2019-11-05 18:02:25 +0000

    Implementation of the Account Activity API
    This has been in use as a sub classed module for around a month. This is taking that sub classed module and dropping it into the tweepy module with barely no difference other than importing the required dependencies.
    
    I have not decorated the individual API calls, as properties. I don't think there's any effective change whether they are used in this context or not.
    
    In retrospect I should have been more consistent in the naming of the functions.

---
+++ tweepy/activityAPI.py
@@ -0,0 +1,159 @@
+# Extensions to Tweepy for the Account Activity API      
+# Licence as LICENSE file.
+# Simon Lucy (bpluly) 
+
+import tweepy
+import logging
+import logging.handlers
+
+from tweepy.binder import bind_api
+from tweepy.error import TweepError
+from tweepy.parsers import JSONParser, Parser
+
+
+
+  
+class ActivityAPI(tweepy.API):
+      pass
+      
+
+      def enable_activity(self, *args, **kwargs):
+          """ :reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'url', 'env'
+          """
+          post_data = {}
+          env = kwargs.pop('env')
+          if env is not None:
+            apiPath = '/account_activity/all/'+env+'/webhooks.json'
+          else:
+            apiPath = '/account_activity/all/webhooks.json'
+          
+          logger.info(apiPath)
+          self.cache=None
+          self.use_cache=False
+          self.parser=JSONParser()
+          return bind_api(
+              api=self,
+              path=apiPath,
+              method='POST',
+              payload_type='status',
+              allowed_param=['url'],
+              require_auth=True,
+              use_cache=False,
+          )(post_data=post_data, *args, **kwargs)        
+      
+      def subscribe_activity(self, *args, **kwargs):
+          """ :reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'url', 'env'
+          """
+          post_data = {}
+          env = kwargs.pop("env")
+          apiPath = '/account_activity/all/'+env+'/subscriptions.json'
+
+          return bind_api(
+              api=self,
+              path=apiPath,
+              method='POST',
+              payload_type='status',
+              require_auth=True
+          )(post_data=post_data, *args, **kwargs)        
+          
+      def subscription(self, *args, **kwargs):
+          """ :reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'url', 'env'
+          """
+          post_data = {}
+          logger.info(kwargs)
+          env = kwargs.pop('env')
+          if env is not None:
+            apiPath = '/account_activity/all/'+env+'/subscriptions.json'
+          else:
+            apiPath = '/account_activity/all/subscriptions.json'
+
+          return bind_api(api=self, path=apiPath, method='GET', payload_type='status', require_auth=True, use_cache=False)()
+                                      
+      
+      def getWebID(self, *args, **kwargs):
+          """:reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'env'. 
+              :returns: an array of JSON. The Twitter documentation is out of date the current format is
+              [{"id":"9999999999999999","url":"https://api.blah-blah/webhook","valid":true,"created_timestamp":"2019-09-16 16:47:16 +0000"}] 
+          """
+          post_data = {}
+          logger.info(kwargs)
+          env = kwargs.pop('env')
+          if env is not None:
+            apiPath = '/account_activity/all/'+env+'/webhooks.json'
+          else:
+            apiPath = '/account_activity/all/webhooks.json'
+
+          return bind_api(api=self, path=apiPath, method='GET', payload_type='json', require_auth=True, use_cache=False)()
+
+      def putWebID(self, *args, **kwargs):
+          """:reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'env'. 
+              :returns: an array of JSON. The Twitter documentation is out of date the current format is
+              [{"id":"9999999999999999","url":"https://api.blah-blah/webhook_id","valid":true,"created_timestamp":"2019-09-16 16:47:16 +0000"}] 
+          """
+          post_data = {}
+          logger.info(kwargs)
+          env = kwargs.pop('env')
+          if env is not None:
+            apiPath = '/account_activity/all/'+env+'/webhook_id.json'
+          else:
+            apiPath = '/account_activity/all/webhook_id.json'
+
+          return bind_api(api=self, path=apiPath, method='PUT', payload_type='json', require_auth=True, use_cache=False)()
+                    
+      def getSubsUsers(self, *args, **kwargs):
+          """:reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'env'. 
+              :returns: an array of JSON. 
+          """
+          post_data = {}
+          logger.info(kwargs)
+          env = kwargs.pop('env')
+          if env is not None:
+            apiPath = '/account_activity/all/'+env+'/subscriptions/list.json'
+          else:
+            apiPath = '/account_activity/all/subscriptions/list.json'
+
+          return bind_api(api=self, path=apiPath, method='GET', payload_type='json', require_auth=True, use_cache=False)()
+ 
+      def getSubsCount(self, *args, **kwargs):
+          """:reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/aaa-premium
+              :allowed_param:'env'. 
+              :returns: an array of JSON. 
+          """
+          post_data = {}
+          logger.info(kwargs)
+          env = kwargs.pop('env')
+          if env is not None:
+            apiPath = '/account_activity/all/subscriptions/count.json'
+          else:
+            apiPath = '/account_activity/all/subscriptions/count.json'
+
+          return bind_api(api=self, path=apiPath, method='GET', payload_type='json', require_auth=True, use_cache=False)()
+                              
+      def replay(self, *args, **kwargs):
+          """ :reference: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/api-reference/replay-api
+              :allowed_param:'fromDate', 'toDate'. 'env', format is fromDate=yyyymmddhhmm toDate=yyyymmddhhmm
+          """
+          post_data = {}
+          webhookid = kwargs.pop('webhookid')
+
+          apiPath = '/account_activity/replay/webhooks/'+webhookid+'/subscriptions/all.json'
+          
+          logger.info(apiPath)
+          self.cache=None
+          self.use_cache=False
+          self.parser=JSONParser()
+          return bind_api(
+              api=self,
+              path=apiPath,
+              method='POST',
+              payload_type='status',
+              allowed_param=['fromDate', 'toDate'],
+              require_auth=True,
+              use_cache=False,
+          )(post_data=post_data, *args, **kwargs)                                  
