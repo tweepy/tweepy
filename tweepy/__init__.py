661ccf214ca0cd08905ef64aa43a10220a257d18
Simon Lucy <simon.lucy@gravit.as>
2019-11-06 11:41:13 +0000

    Make the change in __init__py
    Add the activityAPY file and the ActivityAPI cl;ass to the tweepy class init.
    
    NOT updating the __version__ that would need doing on merging and making any kind of release. For testing bump the version  locally.

--- tweepy/__init__.py
+++ tweepy/__init__.py
@@ -10,6 +10,7 @@ __author__ = 'Joshua Roesslein'
 __license__ = 'MIT'
 
 from tweepy.api import API
+from tweepy.activityAPI import ActivityAPI    
 from tweepy.auth import AppAuthHandler, OAuthHandler
 from tweepy.cache import Cache, FileCache, MemoryCache
 from tweepy.cursor import Cursor
