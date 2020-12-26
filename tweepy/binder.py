e36cf1f90f394111b2a1d628e1c968afd1a6cd76
Harmon <Harmon758@gmail.com>
2020-12-26 01:49:17 -0600

    Update urllib.parse.quote and urllib.parse.urlencode imports

--- tweepy/binder.py
+++ tweepy/binder.py
@@ -9,7 +9,7 @@ import time
 
 import requests
 import six
-from six.moves.urllib.parse import quote, urlencode
+from urllib.parse import quote, urlencode
 
 from tweepy.error import is_rate_limit_error_message, RateLimitError, TweepError
 from tweepy.models import Model
