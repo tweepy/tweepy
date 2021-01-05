a588ecac8819232e8badf27aed3959b5cae5fbc2
Harmon <Harmon758@gmail.com>
2021-01-05 02:54:51 -0600

    Remove explicit handling of requests.ReadTimeout in Stream
    requests.ReadTimeout subclasses requests.Timeout, so this shouldn't be necessary

--- tweepy/streaming.py
+++ tweepy/streaming.py
@@ -247,8 +247,7 @@ class Stream:
                         self.snooze_time = self.snooze_time_step
                         self.listener.on_connect()
                         self._read_loop(resp)
-                except (requests.ReadTimeout, requests.Timeout,
-                        ssl.SSLError) as exc:
+                except (requests.Timeout, ssl.SSLError) as exc:
                     # This is still necessary, as a SSLError can actually be
                     # thrown when using Requests
                     # If it's not time out treat it like any other exception
