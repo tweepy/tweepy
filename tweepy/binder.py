43f371c41fd4e961659985b803cc24ad48286a6e
Harmon <Harmon758@gmail.com>
2021-02-03 11:10:36 -0600

    Remove urllib.parse.quote import in binder.py

--- tweepy/binder.py
+++ tweepy/binder.py
@@ -7,7 +7,7 @@ import sys
 import time
 
 import requests
-from urllib.parse import quote, urlencode
+from urllib.parse import urlencode
 
 from tweepy.error import is_rate_limit_error_message, RateLimitError, TweepError
 from tweepy.models import Model
