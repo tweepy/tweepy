4626c424aaca20b4fcb87bbd590cdb33abed9fb0
Harmon <Harmon758@gmail.com>
2021-02-09 22:11:27 -0600

    Stop allowing positional arguments besides id for API.unretweet

--- tweepy/api.py
+++ tweepy/api.py
@@ -412,11 +412,11 @@ class API:
         )
 
     @payload('status')
-    def unretweet(self, id, *args, **kwargs):
+    def unretweet(self, id, **kwargs):
         """ :reference: https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-unretweet-id
         """
         return self.request(
-            'POST', f'statuses/unretweet/{id}', *args, endpoint_parameters=(
+            'POST', f'statuses/unretweet/{id}', endpoint_parameters=(
                 'trim_user',
             ), **kwargs
         )
