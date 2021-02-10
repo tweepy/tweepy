a2681ed376246677b48156fac66c75d05afdc3ff
Harmon <Harmon758@gmail.com>
2021-02-09 22:19:20 -0600

    Remove id endpoint parameter for API.get_user

--- tweepy/api.py
+++ tweepy/api.py
@@ -448,7 +448,7 @@ class API:
         """
         return self.request(
             'GET', 'users/show', *args, endpoint_parameters=(
-                'id', 'user_id', 'screen_name'
+                'user_id', 'screen_name'
             ), **kwargs
         )
 
