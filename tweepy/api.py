aba327e2aaec8ac5b63c84bdb2ece0bf7e84103f
Harmon <Harmon758@gmail.com>
2021-02-11 22:01:37 -0600

    Add stringify_ids and count endpoint parameters for API.friends_ids

--- tweepy/api.py
+++ tweepy/api.py
@@ -602,7 +602,7 @@ class API:
         """
         return self.request(
             'GET', 'friends/ids', *args, endpoint_parameters=(
-                'user_id', 'screen_name', 'cursor'
+                'user_id', 'screen_name', 'cursor', 'stringify_ids', 'count'
             ), **kwargs
         )
 
