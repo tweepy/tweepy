7299362474563b7082f934c56b150d3f14366e26
Harmon <Harmon758@gmail.com>
2021-02-13 16:13:00 -0600

    Add endpoint parameters for API.destroy_block
    Add include_entities and skip_status endpoint parameters for API.destroy_block

--- tweepy/api.py
+++ tweepy/api.py
@@ -786,7 +786,7 @@ class API:
         """
         return self.request(
             'POST', 'blocks/destroy', *args, endpoint_parameters=(
-                'user_id', 'screen_name'
+                'user_id', 'screen_name', 'include_entities', 'skip_status'
             ), **kwargs
         )
 
