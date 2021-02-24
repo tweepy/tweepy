a8dd7278ac4fcd542717caf6e8a58480a47886ab
Harmon <Harmon758@gmail.com>
2021-02-23 21:57:43 -0600

    Properly pass query for Client.search_all_tweets

--- tweepy/client.py
+++ tweepy/client.py
@@ -286,6 +286,7 @@ class Client:
         Search Tweets: Full-archive search
         https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-all
         """
+        params["query"] = query
         return self._make_request(
             "GET", "/2/tweets/search/all", params=params,
             endpoint_parameters=(
