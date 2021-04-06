f5eca2c24e611876ed9312f260eb8cdc2b9a7ce7
Harmon <Harmon758@gmail.com>
2021-04-06 18:02:08 -0500

    Use Intersphinx linking to link to Python documentation

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,12 @@ sys.path.append(os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.intersphinx'
+]
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
