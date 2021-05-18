87236c1452892d55451149dc8a0fbd386e020631
Harmon <Harmon758@gmail.com>
2021-05-18 15:13:15 -0500

    Switch from recommonmark to MyST-Parser for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-  'recommonmark',
+  'myst_parser',
   'sphinx.ext.autodoc',
   'sphinx.ext.intersphinx',
   'sphinx.ext.napoleon'
