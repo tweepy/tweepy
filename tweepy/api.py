0074bda4620f5e251c504be19534d991b148becd
Harmon <Harmon758@gmail.com>
2021-05-22 12:46:53 -0500

    Update and improve documentation for API.retweet

--- tweepy/api.py
+++ tweepy/api.py
@@ -974,12 +974,20 @@ class API:
 
         Retweets a Tweet. Requires the ID of the Tweet you are retweeting.
 
-        :param id: |sid|
-        :param trim_user: |trim_user|
+        Parameters
+        ----------
+        id
+            |sid|
+        trim_user
+            |trim_user|
 
-        :rtype: :class:`Status` object
+        Returns
+        -------
+        :class:`~tweepy.models.Status`
 
-        :reference: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id
+        References
+        ----------
+        https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-retweet-id
         """
         return self.request(
             'POST', f'statuses/retweet/{id}', endpoint_parameters=(
