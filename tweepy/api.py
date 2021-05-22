3226643cda6c74365008736aafb758fd7008db9d
Harmon <Harmon758@gmail.com>
2021-05-22 06:55:40 -0500

    Update and improve documentation for API.search_30_day

--- tweepy/api.py
+++ tweepy/api.py
@@ -291,24 +291,30 @@ class API:
 
         Premium search that provides Tweets posted within the last 30 days.
 
-        :param label: The (case-sensitive) label associated with your search
-            developer environment, as displayed at
+        Parameters
+        ----------
+        label
+            The (case-sensitive) label associated with your search developer
+            environment, as displayed at
             https://developer.twitter.com/en/account/environments.
-        :param query: The equivalent of one premium rule/filter, with up to
-            1,024 characters (256 with Sandbox dev environments).
+        query
+            The equivalent of one premium rule/filter, with up to 1,024
+            characters (256 with Sandbox dev environments).
 
             This parameter should include ALL portions of the rule/filter,
             including all operators, and portions of the rule should not be
             separated into other parameters of the query.
-        :param tag: Tags can be used to segregate rules and their matching data
-            into different logical groups. If a rule tag is provided, the rule
-            tag is included in the 'matching_rules' attribute.
+        tag
+            Tags can be used to segregate rules and their matching data into
+            different logical groups. If a rule tag is provided, the rule tag
+            is included in the 'matching_rules' attribute.
 
             It is recommended to assign rule-specific UUIDs to rule tags and
             maintain desired mappings on the client side.
-        :param fromDate: The oldest UTC timestamp (from most recent 30 days)
-            from which the Tweets will be provided. Timestamp is in minute
-            granularity and is inclusive (i.e. 12:00 includes the 00 minute).
+        fromDate
+            The oldest UTC timestamp (from most recent 30 days) from which the
+            Tweets will be provided. Timestamp is in minute granularity and is
+            inclusive (i.e. 12:00 includes the 00 minute).
 
             Specified: Using only the fromDate with no toDate parameter will
             deliver results for the query going back in time from now( ) until
@@ -321,10 +327,10 @@ class API:
             If neither the fromDate or toDate parameter is used, the API will
             deliver all results for the most recent 30 days, starting at the
             time of the request, going backwards.
-        :param toDate: The latest, most recent UTC timestamp to which the
-            Tweets will be provided. Timestamp is in minute granularity and is
-            not inclusive (i.e. 11:59 does not include the 59th minute of the
-            hour).
+        toDate
+            The latest, most recent UTC timestamp to which the Tweets will be
+            provided. Timestamp is in minute granularity and is not inclusive
+            (i.e. 11:59 does not include the 59th minute of the hour).
 
             Specified: Using only the toDate with no fromDate parameter will
             deliver the most recent 30 days of data prior to the toDate.
@@ -336,17 +342,23 @@ class API:
             If neither the fromDate or toDate parameter is used, the API will
             deliver all results for the entire 30-day index, starting at the
             time of the request, going backwards.
-        :param maxResults: The maximum number of search results to be returned
-            by a request. A number between 10 and the system limit (currently
-            500, 100 for Sandbox environments). By default, a request response
-            will return 100 results.
-        :param next: This parameter is used to get the next 'page' of results.
-            The value used with the parameter is pulled directly from the
-            response provided by the API, and should not be modified.
+        maxResults
+            The maximum number of search results to be returned by a request. A
+            number between 10 and the system limit (currently 500, 100 for
+            Sandbox environments). By default, a request response will return
+            100 results.
+        next
+            This parameter is used to get the next 'page' of results. The value
+            used with the parameter is pulled directly from the response
+            provided by the API, and should not be modified.
 
-        :rtype: list of :class:`Status` objects
+        Returns
+        -------
+        List[Status]
 
-        :reference: https://developer.twitter.com/en/docs/twitter-api/premium/search-api/api-reference/premium-search
+        References
+        ----------
+        https://developer.twitter.com/en/docs/twitter-api/premium/search-api/api-reference/premium-search
         """
         return self.request(
             'GET', f'tweets/search/30day/{label}', endpoint_parameters=(
