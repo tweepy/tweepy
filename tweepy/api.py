f0c6a88091532641feb52e989dd5a9d499aec9de
Harmon <Harmon758@gmail.com>
2021-05-24 08:57:14 -0500

    Update and improve documentation for API.remove_list_member

--- tweepy/api.py
+++ tweepy/api.py
@@ -1837,16 +1837,28 @@ class API:
         Removes the specified member from the list. The authenticated user must
         be the list's owner to remove members from the list.
 
-        :param list_id: |list_id|
-        :param slug: |slug|
-        :param user_id: |user_id|
-        :param screen_name: |screen_name|
-        :param owner_screen_name: |owner_screen_name|
-        :param owner_id: |owner_id|
+        Parameters
+        ----------
+        list_id
+            |list_id|
+        slug
+            |slug|
+        user_id
+            |user_id|
+        screen_name
+            |screen_name|
+        owner_screen_name
+            |owner_screen_name|
+        owner_id
+            |owner_id|
 
-        :rtype: :class:`List` object
+        Returns
+        -------
+        :class:`~tweepy.models.List`
 
-        :reference: https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/create-manage-lists/api-reference/post-lists-members-destroy
+        References
+        ----------
+        https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/create-manage-lists/api-reference/post-lists-members-destroy
         """
         return self.request(
             'POST', 'lists/members/destroy', endpoint_parameters=(
