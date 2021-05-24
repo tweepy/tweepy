4a4fce05fe8f4323cadacc341ab6ca58b9cd2f46
Harmon <Harmon758@gmail.com>
2021-05-24 08:09:20 -0500

    Update and improve documentation for API.add_list_member

--- tweepy/api.py
+++ tweepy/api.py
@@ -1754,16 +1754,28 @@ class API:
         Add a member to a list. The authenticated user must own the list to be
         able to add members to it. Lists are limited to 5,000 members.
 
-        :param list_id: |list_id|
-        :param slug: |slug|
-        :param user_id: |user_id|
-        :param screen_name: |screen_name|
-        :param owner_screen_name: |owner_screen_name|
-        :param owner_id: |owner_id|
+        Parameters
+        ----------
+        list_id
+            |list_id|
+        slug
+            |slug|
+        user_id
+            |user_id|
+        screen_name
+            |screen_name|
+        owner_screen_name
+            |owner_screen_name|
+        owner_id
+            |owner_id|
 
-        :rtype: :class:`List` object
+        Returns
+        -------
+        :class:`~tweepy.models.List`
 
-        :reference: https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/create-manage-lists/api-reference/post-lists-members-create
+        References
+        ----------
+        https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/create-manage-lists/api-reference/post-lists-members-create
         """
         return self.request(
             'POST', 'lists/members/create', endpoint_parameters=(
