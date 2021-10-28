5bf24464b00257a9fa5f66047a2f7815c1e4f8fb
Harmon <Harmon758@gmail.com>
2021-10-28 10:25:22 -0500

    Fix parse_datetime to parse API datetime string format with Python 3.6
    The '%z' directive didn't accept 'Z' until Python 3.7

--- tweepy/utils.py
+++ tweepy/utils.py
@@ -12,5 +12,6 @@ def list_to_csv(item_list):
 
 def parse_datetime(datetime_string):
     return datetime.datetime.strptime(
-        datetime_string, "%Y-%m-%dT%H:%M:%S.%f%z"
-    )
+        datetime_string, "%Y-%m-%dT%H:%M:%S.%fZ"
+    ).replace(tzinfo=datetime.timezone.utc)
+    # Use %z when support for Python 3.6 is dropped
