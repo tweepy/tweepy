6749e116f47b9307571e016762b3ff918445ba9e
Harmon <Harmon758@gmail.com>
2021-10-30 11:27:33 -0500

    Add Tweet / update status example

---
+++ examples/update_status.py
@@ -0,0 +1,25 @@
+import tweepy
+
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# Tweet / Update Status
+
+# The app and the corresponding credentials must have the Write perission
+
+# Check the App permissions section of the Settings tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+
+# Make sure to reauthorize your app / regenerate your access token and secret 
+# after setting the Write permission
+
+api.update_status("This Tweet was Tweeted using Tweepy!")
