65769166fe435092878f4daf2c15e6a0180939be
Haleelsada <75977159+haleelsada@users.noreply.github.com>
2021-12-04 17:03:57 +0530

    Create search_query_v_1_1.py

---
+++ examples/search_query_v_1_1.py
@@ -0,0 +1,14 @@
+import tweepy
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+for tweet in api.search_tweets(q="search query here",count=50):
+  print(tweet.text)
