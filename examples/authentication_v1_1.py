3f47195f817ca000e26c6dbfc0f5dddb83e778a9
Haleelsada <75977159+haleelsada@users.noreply.github.com>
2021-12-04 12:10:58 +0530

    Update authentication_v1_1.py

--- examples/authentication_v1_1.py
+++ examples/authentication_v1_1.py
@@ -1,16 +1,14 @@
 import tweepy
-
-# Your app's API/consumer key and secret can be found under the Consumer Keys
+# The app's API/consumer key and secret can be found under the Consumer Keys
 # section of the Keys and Tokens tab of your app, under the
 # Twitter Developer Portal Projects & Apps page at
 # https://developer.twitter.com/en/portal/projects-and-apps
 consumer_key = ""
 consumer_secret = ""
 
-# Your account's (the app owner's account's) access token and secret for your
-# app can be found under the Authentication Tokens section of the
-# Keys and Tokens tab of your app, under the
-# Twitter Developer Portal Projects & Apps page at
+# The app's access token and secret can be found under the 
+# Authentication Tokens section of the  Keys and Tokens tab of your app, 
+# under the Twitter Developer Portal Projects & Apps page at
 # https://developer.twitter.com/en/portal/projects-and-apps
 access_token = ""
 access_token_secret = ""
