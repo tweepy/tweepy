d5d50980f03a00c69b3f30650973b65edc6a1072
Haleelsada <75977159+haleelsada@users.noreply.github.com>
2021-12-05 20:48:43 +0530

    Add search query example

--- examples/search_query_v2.py
+++ examples/search_query_v2.py
@@ -3,5 +3,5 @@ import tweepy
 bearer_token = ""
 client = tweepy.Client(bearer_token)
 
-for tweet in client.search_recent_tweets(query = "search query here",max_results=100)[0]:
+for tweet in client.search_recent_tweets(query = "search query here",max_results=50)[0]:
   print(tweet)
