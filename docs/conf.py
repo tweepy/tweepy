1ef43e7c95dca6471d77ff4b80bbcda35a52d1af
Harmon <Harmon758@gmail.com>
2022-02-25 11:01:45 -0600

    Explicitly specify Read the Docs Sphinx Theme for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
