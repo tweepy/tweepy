e1d7e61bfca01eb55067971d3bc8eab205fe6368
Suhem Parack <sparack@gmail.com>
2022-03-01 12:31:19 -0800

    Update examples/API_v2/get_liked_tweets.py
    Co-authored-by: Harmon <Harmon758@gmail.com>

--- examples/API_v2/get_liked_tweets.py
+++ examples/API_v2/get_liked_tweets.py
@@ -10,8 +10,8 @@ client = tweepy.Client(bearer_token)
 # Replace User ID
 id = '2244994945'
 
-# By default the Tweet ID and Tweet text are returned. If you want additional fields, use the appropriate
-# fields and expansions
+# By default the Tweet ID and text are returned. If you want additional fields,
+# request them using the tweet_fields parameter
 tweets = client.get_liked_tweets(id=id, tweet_fields=['context_annotations','created_at'])
 
 for tweet in tweets.data:
