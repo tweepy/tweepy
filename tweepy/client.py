bdd72e9c6425640cca87b98615536fb8389b9911
Harmon <Harmon758@gmail.com>
2022-03-14 14:04:21 -0500

    Fix parameter name in documentation for Client.get_tweets

--- tweepy/client.py
+++ tweepy/client.py
@@ -1328,7 +1328,7 @@ class Client(BaseClient):
     def get_tweets(self, ids, *, user_auth=False, **params):
         """get_tweets( \
             ids, *, expansions=None, media_fields=None, place_fields=None, \
-            poll_fields=None, twitter_fields=None, user_fields=None, \
+            poll_fields=None, tweet_fields=None, user_fields=None, \
             user_auth=False \
         )
 
