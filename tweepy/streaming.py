6e3473a719b67a6260730a4b8fca770bb4707576
Harmon <Harmon758@gmail.com>
2022-04-30 06:23:10 -0500

    Deprecate Stream.sample
    https://twittercommunity.com/t/deprecation-announcement-removing-compliance-messages-from-statuses-filter-and-retiring-statuses-sample-from-the-twitter-api-v1-1/170500

--- tweepy/streaming.py
+++ tweepy/streaming.py
@@ -345,6 +345,11 @@ class Stream(BaseStream):
     def sample(self, *, languages=None, stall_warnings=False, threaded=False):
         """Sample realtime Tweets
 
+        .. deprecated:: 4.9
+            `The Twitter API v1.1 endpoint this method uses is now deprecated
+            and will be retired on October 29, 2022.`_ Twitter API v2 can be
+            used instead with :meth:`StreamingClient.sample`.
+
         Parameters
         ----------
         languages : list[str] | None
@@ -370,6 +375,8 @@ class Stream(BaseStream):
 
         .. _BCP 47: https://tools.ietf.org/html/bcp47
         .. _advanced search: https://twitter.com/search-advanced
+        .. _The Twitter API v1.1 endpoint this method uses is now deprecated
+            and will be retired on October 29, 2022.: https://twittercommunity.com/t/deprecation-announcement-removing-compliance-messages-from-statuses-filter-and-retiring-statuses-sample-from-the-twitter-api-v1-1/170500
         """
         if self.running:
             raise TweepyException("Stream is already connected")
