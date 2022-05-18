8d544103d08b17a48dc9d424db4498184e10d8a3
Harmon <Harmon758@gmail.com>
2022-05-17 23:18:25 -0500

    Check for async_lru when importing asynchronous subpackage

--- tweepy/asynchronous/__init__.py
+++ tweepy/asynchronous/__init__.py
@@ -10,11 +10,13 @@ Asynchronous interfaces with the Twitter API
 
 try:
     import aiohttp
+    import async_lru
     import oauthlib
 except ModuleNotFoundError:
     from tweepy.errors import TweepyException
     raise TweepyException(
-        "tweepy.asynchronous requires aiohttp and oauthlib to be installed"
+        "tweepy.asynchronous requires aiohttp, async_lru, and oauthlib to be "
+        "installed"
     )
 
 from tweepy.asynchronous.streaming import AsyncStream
