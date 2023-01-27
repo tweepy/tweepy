c704d80e86f823fbe1607078983cca5553bd398c
Francesco Di Lorenzo <francesco@typefully.com>
2023-01-27 10:31:01 +0000

    Fix KeyError in chunked upload method
    The Twitter API has stopped reliably returning the 'check_after_secs' key-value pair in their response to the chunked media upload endpoint.

--- tweepy/api.py
+++ tweepy/api.py
@@ -3685,7 +3685,7 @@ class API:
 
         if wait_for_async_finalize and hasattr(media, 'processing_info'):
             while media.processing_info['state'] in ('pending', 'in_progress'):
-                time.sleep(media.processing_info['check_after_secs'])
+                time.sleep(media.processing_info.get('check_after_secs', 5))
                 media = self.get_media_upload_status(media.media_id, **kwargs)
 
         return media
