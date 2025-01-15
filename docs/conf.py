b382ef9feaa4864d0e954ff1720a7b2f54c9ddb8
Josh Roesslein <jroesslein@gmail.com>
2025-01-15 17:49:13 -0600

    Fix Sphinx context injection removal by readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,13 @@ import sys
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 
+# Set canonical URL from the Read the Docs Domain
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
+
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
